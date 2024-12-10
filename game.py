7e0f9136c90df4fba36ea8a6fd61656f13383c4b
noahkaboa <nphinger@gmail.com>
2024-12-10 14:11:35 -0500

    Added str to little board + begun big board

--- game.py
+++ game.py
@@ -1,13 +1,20 @@
 class LittleBoard:
     def __init__(self):
         self.board = [
-            [""]*3,
-            [""]*3,
-            [""]*3
+            ["_"]*3,
+            ["_"]*3,
+            ["_"]*3
         ]
         self.status = ""
         self.player_alternator = self.change_players()
         self.to_move = self.player_alternator.__next__()
+
+    def __str__(self):
+        s = ""
+        for row in self.board:
+            s += "-".join(row)
+            s += "\n"
+        return s
 
     def check_rows(self):
         for row in self.board:
@@ -46,7 +53,7 @@ class LittleBoard:
         return False
     
     def make_move(self, row, col):
-        if self.board[row][col] != "":
+        if self.board[row][col] != "_":
             return False
         self.board[row][col] = self.to_move
         self.to_move = self.player_alternator.__next__()
@@ -56,18 +63,38 @@ class LittleBoard:
         while True:
             yield "X"
             yield "O"
+
+    def force_move(self, row, col, player):
+        if self.board[row][col] == "_":
+            self.board[row][col] = player
+
+
+class BigBoard:
+    def __init__(self):
+        self.big_board = [[LittleBoard()] * 3] * 3
+        self.game_state = LittleBoard()
+        self.to_move = "X"
+        self.prev_move = {
+            "small_row": -1,
+            "small_col": -1
+        }
+
     
 
-b = LittleBoard()
+# b = LittleBoard()
 
-while not b.check_winner():
-    print(b.board)
-    print(f"{b.to_move} to move")
+# while not b.check_winner():
+#     print(b.board)
+#     print(f"{b.to_move} to move")
 
-    valid_move = False
-    while not valid_move:
-        row = int(input("Row:\t"))
-        col = int(input("Col:\t"))
-        valid_move = b.make_move(row, col)
-        if not valid_move:
-            print("Invalid Move! try again")
+#     valid_move = False
+#     while not valid_move:
+#         row = int(input("Row:\t"))
+#         col = int(input("Col:\t"))
+#         valid_move = b.make_move(row, col)
+#         if not valid_move:
+#             print("Invalid Move! try again")
+
+
+g = BigBoard()
+print(g.game_state)
