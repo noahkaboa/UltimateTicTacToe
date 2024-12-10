9a81c9dd817efad2145d5c1ff80136d760908c39
noahkaboa <nphinger@gmail.com>
2024-12-10 15:54:13 -0500

    Core gameplay

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import os
+
 class LittleBoard:
     def __init__(self):
         self.board = [
@@ -16,7 +18,7 @@ class LittleBoard:
 
     def check_rows(self):
         for row in self.board:
-            if len(list(set(row))) == 1:
+            if len(list(set(row))) == 1 and not row[0] == "_":
                 return row[0]
         return False
 
@@ -25,16 +27,16 @@ class LittleBoard:
             col = []
             for row in self.board:
                 col.append(row[i])
-            if len(list(set(col))) == 1:
+            if len(list(set(col))) == 1 and not col[0] == "_":
                 return col[0]
         return False
     
     def check_diags(self):
         d1 = [self.board[0][0], self.board[1][1], self.board[2][2]]
-        if len(list(set(d1))) == 1:
+        if len(list(set(d1))) == 1 and not d1[0] == "_":
             return d1[0]
         d2 = [self.board[0][2], self.board[1][1], self.board[2][0]]
-        if len(list(set(d2))) == 1:
+        if len(list(set(d2))) == 1 and not d2[0] == "_":
             return d2[0]
         return False
     
@@ -67,20 +69,36 @@ class BigBoard:
             "small_col": -1
         }
 
+    def __str__(self):
+        rows = []
+        for big_row in range(3):
+            little_rows = [""] * 3
+            for big_col in range(3):
+                little_board = self.big_board[big_row][big_col]
+                for i in range(3):
+                    little_rows[i] += " ".join(little_board.board[i]) + "\t"
+            rows.extend(little_rows)
+            rows.append("")
 
+        return "\n".join(rows)
 
     def make_move(self, big_row, big_col, small_row, small_col):
         if big_row != self.prev_move["small_row"] or big_col != self.prev_move["small_col"]:
             if self.prev_move["small_row"] != -1:
                 return False
+        if self.big_board[big_row][big_col].check_winner():
+            return False
+        
             
         if self.big_board[big_row][big_col].make_move(small_row, small_col, self.to_move):
             self.prev_move["small_row"] = small_row
             self.prev_move["small_col"] = small_col
             
             if self.big_board[big_row][big_col].check_winner():
-                self.prev_move["small_row"] = -1
                 self.game_state.make_move(big_row, big_col, self.to_move)
+
+            if self.big_board[small_row][small_col].check_winner():
+                self.prev_move["small_row"] = -1
 
             self.to_move = "X" if self.to_move == "O" else "O"
             
@@ -103,11 +121,51 @@ class BigBoard:
 #     while not valid_move:
 #         row = int(input("Row:\t"))
 #         col = int(input("Col:\t"))
-#         valid_move = b.make_move(row, col)
+#         valid_move = b.make_move(row, col, b.to_move)
 #         if not valid_move:
 #             print("Invalid Move! try again")
 
 
 g = BigBoard()
-print(g.game_state)
-print(g)
+
+while not g.game_state.check_winner() in ["X", "O"]:
+    os.system('cls')
+    print(g.game_state)
+    print(g)
+    print(f"{g.to_move} to move")
+    print(f"{g.prev_move=}")
+
+    
+    valid_move = False
+    while not valid_move:
+        big_row = input("Big Row:\t")
+        if big_row.isdigit():
+            big_row = int(big_row)
+        else:
+            print("Invalid Move! Try again")
+            continue
+        big_col = input("Big Col:\t")
+        if big_col.isdigit():
+            big_col = int(big_col)
+        else:
+            print("Invalid Move! Try again")
+            continue
+        small_row = input("Small Row:\t")
+        if small_row.isdigit():
+            small_row = int(small_row)
+        else:
+            print("Invalid Move! Try again")
+            continue
+        small_col = input("Small Col:\t")
+        if small_col.isdigit():
+            small_col = int(small_col)
+        else:
+            print("Invalid Move! Try again")
+            continue
+        
+        valid_move = g.make_move(big_row, big_col, small_row, small_col)
+        if not valid_move:
+            print("Invalid Move! Try again")
+
+print("Congrats!")
+print(g.game_state.check_winner())
