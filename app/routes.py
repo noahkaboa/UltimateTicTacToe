9ff99a5ae914198d8675b4bb33f32eb878862d08
noahkaboa <nphinger@gmail.com>
2024-12-10 17:56:17 -0500

    added game

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,21 @@
 from flask import render_template, request, redirect, url_for
 from app import app
+from game import Game
+
+g = Game()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_player=g.board.to_move)
+
+@app.route('/play', methods=['POST'])
+def play():
+    pass
+
+@app.route('/get_board', methods=['GET'])
+def get_board():
+    pass
+
+@app.route('/reset', methods=['POST'])
+def reset():
+    g = Game()
