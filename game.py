02efc9cd364371ec08f1265b48987c51324314f6
noahkaboa <nphinger@gmail.com>
2024-12-14 12:32:54 -0500

    Ultimate TicTacToe Notation

--- game.py
+++ game.py
@@ -106,47 +106,84 @@ class BigBoard:
         
     def is_valid_move(self, big_row, big_col, small_row, small_col):
         return self.big_board[big_row][big_col].board[small_row][small_col] == "_" and not self.big_board[big_row][big_col].check_winner() and (self.prev_move["small_row"] == -1 or (big_row == self.prev_move["small_row"] and big_col == self.prev_move["small_col"]))
-        
-class Game:
-    def __init__(self):
-        self.board = BigBoard()
-
-    def play(self):
-        while not self.board.game_state.check_winner() in ["X", "O"]:
-            print(self.board.game_state)
-            print(self.board)
-            print(f"{self.board.to_move} to move")
-            print(f"{self.board.prev_move=}")
-
-            
-            valid_move = False
-            while not valid_move:
-                big_row = input("Big Row:\t")
-                if big_row.isdigit():
-                    big_row = int(big_row)
-                else:
-                    print("Invalid Move! Try again")
-                    continue
-                big_col = input("Big Col:\t")
-                if big_col.isdigit():
-                    big_col = int(big_col)
-                else:
-                    print("Invalid Move! Try again")
-                    continue
-                small_row = input("Small Row:\t")
-                if small_row.isdigit():
-                    small_row = int(small_row)
-                else:
-                    print("Invalid Move! Try again")
-                    continue
-                small_col = input("Small Col:\t")
-                if small_col.isdigit():
-                    small_col = int(small_col)
-                else:
-                    print("Invalid Move! Try again")
-                    continue
-                
-                valid_move = self.board.make_move(big_row, big_col, small_row, small_col)
-                if not valid_move:
-                    print("Invalid Move! Try again")
     
+    def board_to_utn(self):
+        s = ""
+        blank_count = 0
+        for big_row in range(3):
+            for big_col in range(3):
+                for small_row in range(3):
+                    for small_col in range(3):
+                        if self.big_board[big_row][big_col].board[small_row][small_col] == "_":
+                            blank_count += 1
+                        else:
+                            if blank_count != 0:
+                                s += str(blank_count)
+                            blank_count = 0
+                            s += self.big_board[big_row][big_col].board[small_row][small_col]
+        if blank_count != 0:
+            s += str(blank_count)
+
+        s += " " + self.to_move
+        s += " " + str(self.prev_move["small_row"]) + " " + str(self.prev_move["small_col"])
+        return s
+    
+    def utn_parse(self, s):
+        compile = ""
+        complete = []
+        for letter in s:
+            if compile:
+                if compile.isdigit() == letter.isdigit():
+                    compile += letter
+                else:
+                    complete.append(compile)
+                    compile = letter
+            else:
+                compile = letter
+        if compile:
+            complete.append(compile)
+
+        for i in range(len(complete)):
+            if complete[i].isalpha():
+                if len(complete[i]) > 1:
+                    complete[i:i] = [*complete[i]]
+                    complete.pop(i+len(complete[i]) + 1)
+
+        full_board = []
+        for item in complete:
+            if item.isdigit():
+                full_board.extend(["_"]*int(item))
+            else:
+                full_board.append(item)
+        
+        return full_board
+
+    def utn_to_board(self, s):
+        board, player, prev_row, prev_col = s.split(" ")
+        items = self.utn_parse(board)
+        self.to_move = player
+        self.big_board = [[LittleBoard() for _ in range(3)] for _ in range(3)]
+        self.game_state = LittleBoard()
+        self.prev_move = {
+            "small_row": int(prev_row),
+            "small_col": int(prev_col)
+        }
+
+        item = items.pop(0)
+
+        for big_row_i in range(3):
+            for big_col_j in range(3):
+                for small_row_k in range(3):
+                    for small_col_l in range(3):
+                        if items:
+                            if isinstance(item, int):
+                                if item == 0:
+                                    item = items.pop(0)
+                                else:
+                                    item -= 1
+                                    self.big_board[big_row_i][big_col_j].board[small_row_k][small_col_l] = "_"
+                            else:
+                                self.big_board[big_row_i][big_col_j].board[small_row_k][small_col_l] = item
+                                item = items.pop(0)
+                                if item.isdigit():
+                                    item = int(item)
